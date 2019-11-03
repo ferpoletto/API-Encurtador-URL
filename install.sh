53b5539b47f84ed1f3bb9ed04630c3fd70c3ce05
Fernando Poletto <fernandompoletto@gmail.com>
2019-11-03 09:05:24 -0300

    shell

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#sudo apt-get install git
+
+#git clone https://github.com/ferpoletto/Encurtador-URLS-Flask-Postgres.git
+
+
+
 #Instalando e configurando o Postgres
 sudo apt-get install postgresql postgresql-contrib
 
@@ -9,14 +15,13 @@ sudo -u postgres psql -d postgres -c "ALTER USER postgres PASSWORD 'postgres';"
 
 sudo -u postgres psql -d postgres -c create database links;
 
-sudo -u postgres psql -d postgres -c \c links;
-
 sudo -u postgres psql -d postgres -c create table users(id_usuario serial primary key,usuario varchar(30) not null unique);
 
 sudo -u postgres psql -d postgres -c CREATE TABLE links(id serial primary key,hits integer not null,url varchar(100) not null,shorturl varchar(100) not null unique,usuario_id integer not null,FOREIGN KEY (usuario_id) REFERENCES users(id_usuario));
 
 #Instalando o pip
 apt-get install python3-pip
+
 
 #Instalando dependencias
 pip install Flask
