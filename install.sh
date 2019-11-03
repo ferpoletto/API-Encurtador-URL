eac40a7a5b2f85295306ae83297ed8a07b540d1a
Fernando Poletto <fernandompoletto@gmail.com>
2019-11-03 10:38:33 -0300

    shell

--- install.sh
+++ install.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
 
-#sudo apt-get install git
+#INSTALANDO O GIT
+sudo apt-get update
+sudo apt-get install git
+#sudo apt-get upgrade python3
 
-#git clone https://github.com/ferpoletto/Encurtador-URLS-Flask-Postgres.git
-
-
-
-#Instalando e configurando o Postgres
+#INSTALANDO DEPENDENCIAS
 sudo apt-get install postgresql postgresql-contrib
+sudo apt-get install libpq-dev python-dev
 
-sudo -u postgres psql
+#INSTALANDO E CONFIGURANDO O POSTGRES
+postgresql-setup initdb
+sudo -u postgres createdb links
+sudo -u postgres psql -d postgres -c "ALTER USER postgres WITH PASSWORD 'postgres'"
+sudo -u postgres psql -d postgres -c "CREATE TABLE users (id_usuario serial primary key,usuario varchar(30) not null unique)";
+sudo -u postgres psql -d postgres -c "CREATE TABLE links (id serial primary key,hits integer not null,url varchar(100) not null,shorturl varchar(100) not null unique,usuario_id integer not null,FOREIGN KEY (usuario_id) REFERENCES users(id_usuario))";
 
-sudo -u postgres psql -d postgres -c "ALTER USER postgres PASSWORD 'postgres';"
 
-sudo -u postgres psql -d postgres -c create database links;
 
-sudo -u postgres psql -d postgres -c create table users(id_usuario serial primary key,usuario varchar(30) not null unique);
 
-sudo -u postgres psql -d postgres -c CREATE TABLE links(id serial primary key,hits integer not null,url varchar(100) not null,shorturl varchar(100) not null unique,usuario_id integer not null,FOREIGN KEY (usuario_id) REFERENCES users(id_usuario));
 
-#Instalando o pip
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#CLONANDO O REPOSITORIO
+cd /Documentos
+git clone https://github.com/ferpoletto/Encurtador-URLS-Flask-Postgres.git
+cd Encurtador-URLS-Flask-Postgres
+
+
+#INSTALANDO O PIP E VIRTUALENV
 apt-get install python3-pip
+sudo apt-get install virtualenv
+virtualenv --python=python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
 
-
-#Instalando dependencias
-pip install Flask
-pip install psycopg2
 
 
 
--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 cd /Documentos/Projeto/Encurtador-URLS-Flask-Postgres
 
+source venv/bin/activate
+
 python3 run.py
