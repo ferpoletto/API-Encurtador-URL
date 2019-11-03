aa56961b4f57ab17a122753644f5eff261144026
Fernando Poletto <fernandompoletto@gmail.com>
2019-11-03 16:50:10 -0300

    Banco

--- install.sh
+++ install.sh
@@ -13,14 +13,14 @@ postgresql-setup initdb
 sudo -u postgres createdb links
 sudo -u postgres create schema public
 sudo -u postgres psql -d postgres -c "ALTER USER postgres WITH PASSWORD 'postgres'"
-sudo -u postgres psql -d postgres -c "CREATE TABLE users (id_usuario serial primary key,usuario varchar(30) not null unique)";
-sudo -u postgres psql -d postgres -c "CREATE TABLE links (id serial primary key,hits integer not null,url varchar(100) not null,shorturl varchar(100) not null unique,usuario_id integer not null,FOREIGN KEY (usuario_id) REFERENCES users(id_usuario))";
+#sudo -u postgres psql -d postgres -c "CREATE TABLE users (id_usuario serial primary key,usuario varchar(30) not null unique)";
+#sudo -u postgres psql -d postgres -c "CREATE TABLE links (id serial primary key,hits integer not null,url varchar(100) not null,shorturl varchar(100) not null unique,usuario_id integer not null,FOREIGN KEY (usuario_id) REFERENCES users(id_usuario))";
 
 #INSTALANDO O PIP E VIRTUALENV
-apt-get install python3-pip
+sudo apt-get install python3-pip
 sudo apt-get install virtualenv
 virtualenv --python=python3 venv
 source venv/bin/activate
-pip install -r requirements.txtvirutal
+pip install -r requirements.txt
 
 
--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #START NO BANCO DE DADOS
-sudo -u postgres psql -h postgres -U postgres -d links
+#sudo -u postgres psql -h postgres -U postgres -d links
 
 #START NA APLICAÇÃO
+source venv/bin/activate
 python3 run.py
