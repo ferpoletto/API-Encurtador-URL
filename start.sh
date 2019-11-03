6147712ed23b004ac825467ff6f996c5bcccfeaa
Fernando Poletto <fernandompoletto@gmail.com>
2019-11-03 10:52:40 -0300

    shell start

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
 #INSTALANDO O PIP E VIRTUALENV
 apt-get install python3-pip
 sudo apt-get install virtualenv
 virtualenv --python=python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+
+python3 run.py
