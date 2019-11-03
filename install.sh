7492d5330b3610b1a3fb33550d826191b7fda3c0
Fernando Poletto <fernandompoletto@gmail.com>
2019-11-03 12:11:07 -0300

    Script Shell

--- install.sh
+++ install.sh
@@ -15,6 +15,11 @@ sudo -u postgres psql -d postgres -c "ALTER USER postgres WITH PASSWORD 'postgre
 sudo -u postgres psql -d postgres -c "CREATE TABLE users (id_usuario serial primary key,usuario varchar(30) not null unique)";
 sudo -u postgres psql -d postgres -c "CREATE TABLE links (id serial primary key,hits integer not null,url varchar(100) not null,shorturl varchar(100) not null unique,usuario_id integer not null,FOREIGN KEY (usuario_id) REFERENCES users(id_usuario))";
 
-
+#INSTALANDO O PIP E VIRTUALENV
+apt-get install python3-pip
+sudo apt-get install virtualenv
+virtualenv --python=python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
 
 
--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-#INSTALANDO O PIP E VIRTUALENV
-apt-get install python3-pip
-sudo apt-get install virtualenv
-virtualenv --python=python3 venv
-source venv/bin/activate
-pip install -r requirements.txt
-
-
 #START NO BANCO DE DADOS
-psql -h 127.0.0.1 -U postgres -d links
+sudo -u postgres psql -h 127.0.0.1 -U postgres -d links
 
 #START NA APLICAÇÃO
 python3 run.py
